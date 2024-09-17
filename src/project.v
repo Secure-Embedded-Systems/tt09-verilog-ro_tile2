0550d26398e4efe8a2c8dd83c654447a77bb952a
Patrick Schaumont <patrickschaumont@users.noreply.github.com>
2024-09-17 18:05:12 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_example (
      if (rst_n == 1'b0)
        counter <= 8'b0;
      else
-       counter <= counter + 8'b1;
+       counter <= counter - 8'b1;
 
    assign uo_out   = counter;
    assign uio_oe   = 8'b0;
