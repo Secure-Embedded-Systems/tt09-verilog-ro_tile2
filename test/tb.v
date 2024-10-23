13ae5d9e1de8b2fd818aa4bfa1b90fc15d32bd7d
Roy1707018 <aroy1@wpi.edu>
2024-10-23 02:33:52 -0400

    gl testing

--- test/tb.v
+++ test/tb.v
@@ -1,4 +1,4 @@
-`default_nettype noneInout port expression must support continuous assignment.
+`default_nettype none
 `timescale 1ns / 1ps
 
 /* This testbench just instantiates the module and makes some convenient wires
